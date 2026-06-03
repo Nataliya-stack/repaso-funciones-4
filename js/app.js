3e0ea2e97bf4c345f28ee41d84a74e5d4472163a
Natalia <nataliya.yanovic@mail.ru>
2026-06-03 09:23:46 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const repeatTexto = (texto, numero) => texto.repeat(numero);
+
+const inputEl = document.getElementById("inputTexto");
+const inputNum = document.getElementById("inputNumero")
+const btnEl = document.getElementById("btnCalcular");
+const resultEl = document.getElementById("resultadoTexto");
+
+btnEl.addEventListener("click", () => {
+    const texto = inputEl.value;
+    const numero = parseInt(inputNum.value);
+
+    if (texto === "" || isNaN(numero) || numero <= 0) {
+        resultEl.textContent = "Error: ¡Los campos no pueden estar vacío!";        
+        resultEl.style.color = "red";
+        return; 
+    }
+
+    resultEl.innerHTML = `${repeatTexto(texto + "<br>", numero)}`;
+    resultEl.style.color = "white";
+});
